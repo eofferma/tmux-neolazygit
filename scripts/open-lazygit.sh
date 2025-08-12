98edc73f9aa137e222f136e7a57462496c246f13
un_9bot <un.9bot@gmail.com>
2025-08-12 15:57:24 +0300

    feat-switch-exist - feat: switch to exist pane

--- scripts/open-lazygit.sh
+++ scripts/open-lazygit.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CUSTOM_LAZYGIT_CONFIG="$CURRENT_DIR/../lazygit/config.yml"
+LAZYGIT_EDITOR="$CURRENT_DIR/editor.sh"
+LAZYGIT_CONFIG="$(lazygit -cd)/config.yml"
 
-LAZYGIT_EDITOR="$CURRENT_DIR/editor.sh" # Check usage in lazygit/config.yml
-LAZYGIT_CONFIG=$(echo "$(lazygit -cd)/config.yml")
+openLazygit() {
+  local ORIGIN_PANE
+  ORIGIN_PANE="$(tmux display-message -p "#D")"
 
-openLazygit () {
-    # Gets the pane id from where the script was called
-    local LAZYGIT_ORIGIN_PANE=($(tmux display-message -p "#D"))
-    
-    # Get the current working directory of the pane
-    local CURRENT_PATH=$(tmux display-message -p -t $LAZYGIT_ORIGIN_PANE "#{pane_current_path}")
+  local CURRENT_PATH
+  CURRENT_PATH="$(tmux display-message -p -t "$ORIGIN_PANE" "#{pane_current_path}")"
 
-    # Opens a new tmux window running lazygit appending the needed config
-    tmux neww -c "$CURRENT_PATH" \
-        -e LAZYGIT_EDITOR=$LAZYGIT_EDITOR \
-        -e LAZYGIT_ORIGIN_PANE=$LAZYGIT_ORIGIN_PANE \
-        lazygit \
-        -ucf "$LAZYGIT_CONFIG,$CUSTOM_LAZYGIT_CONFIG"
+  local WINDOW_NAME
+  WINDOW_NAME="LG-${ORIGIN_PANE//%/}"
+
+  # check exist window
+  if tmux list-windows -F "#{window_name}" | grep -Fxq "$WINDOW_NAME"; then
+    # switch to window
+    tmux select-window -t "$WINDOW_NAME"
+  else
+    # else create new window
+    tmux neww -n "$WINDOW_NAME" -c "$CURRENT_PATH" \
+      -e LAZYGIT_EDITOR="$LAZYGIT_EDITOR" \
+      -e LAZYGIT_ORIGIN_PANE="$ORIGIN_PANE" \
+      lazygit \
+      -ucf "$LAZYGIT_CONFIG,$CUSTOM_LAZYGIT_CONFIG"
+  fi
 }
 
 openLazygit
